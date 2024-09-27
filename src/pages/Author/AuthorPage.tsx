3b938c947756240f782ec254839de2157f6a5698
williamh973 <126678879+williamh973@users.noreply.github.com>
2024-09-27 16:47:27 +0200

    contact form functions added

--- src/pages/Author/AuthorPage.tsx
+++ src/pages/Author/AuthorPage.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Button } from "../../components/ui/button/Button";
 
 export function AuthorPage() {
+  const [author, setAuthor] = useState({
+    firstname: "",
+    lastname: "",
+  });
+
+  function handleChange(e: any) {
+    const { name, value } = e.target;
+    setAuthor({ ...author, [name]: value });
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log("Auteur : ", author);
+  }
+
   return (
-    <form>
-      <input type="text" name="firstname" />
-      <input type="text" name="lastname" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="firstname" onChange={handleChange} />
+      <input type="text" name="lastname" onChange={handleChange} />
 
       <Button label="Créer l'auteur" type="submit" onClick={() => {}} />
     </form>
